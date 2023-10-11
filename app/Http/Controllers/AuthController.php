4b6e8754fa174684b52fcad8a9fdeff8b471d409
DESKTOP-RSQRIA7\User <alexander.goyens@gmail.com>
2023-10-11 18:14:25 +0200

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            
+            // Generate access token for user
+            $token = $user->createToken('MyApp')->accessToken;
+
+            return response()->json(['access_token' => $token, 'user' => $user]);
+        } else {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+    }
+}
